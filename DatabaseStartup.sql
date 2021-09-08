15ae5d2da8543aab046d301f919460afc2afec33
terrytrl <73771668+terrytrl@users.noreply.github.com>
2021-09-08 22:19:59 +1000

    Update DatabaseStartup.sql

--- DatabaseStartup.sql
+++ DatabaseStartup.sql
@@ -1,5 +1,3 @@
-create schema group420;
-
 create table CustomerOrder (
 	Transaction_number int (10),
     Item_name varchar(50),
@@ -10,9 +8,19 @@ create table CustomerOrder (
     Time_Of_Sale Time
 );
 
+
+drop table Warehouse;
+
 create table Warehouse (
 	Item_ID int,
+    Item_Name varchar(50), 
     Quantity int (4),
     Item_Description varchar(50),
     Price int(4)
 );
+
+insert into Warehouse (
+	Item_ID, Item_Name, Quantity, Item_Description, Price
+) values (1, "Four Twenty", 1, "slight yeeeting", 420);
+
+select * from Warehouse;
