ab13a803f26c4dad5a624c19768a2206dc339321
terrytrl <73771668+terrytrl@users.noreply.github.com>
2021-09-26 20:18:58 +1000

    Updated SQL

--- DatabaseStartup.sql
+++ DatabaseStartup.sql
@@ -1,5 +1,3 @@
-create schema group420;
-
 create table CustomerOrder (
 	Transaction_number int (10),
     Item_name varchar(50),
@@ -10,6 +8,8 @@ create table CustomerOrder (
     Time_Of_Sale Time
 );
 
+
+drop table Warehouse;
 
 create table Warehouse (
 	Item_ID int,
@@ -26,7 +26,28 @@ insert into Warehouse (
 
 insert into Warehouse (
 	Item_ID, Item_Name, Quantity, Item_Description, Price
-) values (2, "", 1, "slight yeeeting", 420);
+) values (2, "Fire Crackers", 1, "Highly Flamable", 35);
 
+insert into Warehouse (
+	Item_ID, Item_Name, Quantity, Item_Description, Price
+) values (3, "water", 1, "H2O", 10);
+
+
+insert into Warehouse (
+	Item_ID, Item_Name, Quantity, Item_Description, Price
+) values (4, "Ear Muffs", 1, "Fluffy", 45);
+
+insert into Warehouse (
+	Item_ID, Item_Name, Quantity, Item_Description, Price
+) values (5, "bandaids", 1, "Thick", 69);
+
+
+use group420;
 
 select * from Warehouse;
+
+select * from Login_;
+
+update Warehouse
+SET Item_Description = "Thick and lubricated" Where Item_id = 3;
+
