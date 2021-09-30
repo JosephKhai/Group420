08099eb69b0a77fa8ae6e6a57e2fd042fe0479a2
Pum Suan Khai SEKTAK <josephkhaipi@gmail.com>
2021-09-30 17:58:54 +1000

    test

---
+++ homepage.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html>
+
+<head>
+    <title>Home</title>
+    <meta name="description" content="index">
+    <meta name="keywords" content="index">
+    <link rel="stylesheet" href="Style/style.css">
+</head>
+
+<body>
+
+    <div>
+        <h1>Home Page</h1>
+    </div>
+
+
+
+    <!--Home Page-->
+
+    <div class="dashboard">
+        <h3>Pharmacy System Admin page</h3>
+
+
+        <div class="row">
+            <div class="column">
+                <div class="card">
+                    <h3>Card 1</h3>
+
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <h3>Card 2</h3>
+
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <h3>Card 3</h3>
+
+                </div>
+            </div>
+
+            <div class="column">
+                <div class="card">
+                    <h3>Card 4</h3>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>Home</title>
+    <title>Login Page</title>
     <meta name="description" content="index">
     <meta name="keywords" content="index">
     <link rel="stylesheet" href="Style/style.css">
@@ -11,53 +11,57 @@
 <body>
     <ul class="topnav">
         <li><a class="active" href="index.php">Home</a></li>
-        <li><a href="UpdateStock.html">Update Stock</a></li>
-        <li><a href="SalesReport.php">Sales Report</a></li>
         <li class="right"><a href="About.html">About</a></li>
     </ul>
 
     <div>
-        <h1>Home</h1>
+        <h1>Login Page</h1>
     </div>
+
 
     <?php
 
-   
+    function sanitise_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
     //if session is login_user
     if (isset($_SESSION['login_user'])) {
-        header("location: index.php");
+        header("location: manager.php");
     }
+
 
     $error = "";
     require_once "settings.php";    // Load MySQL log in credentials
-    $conn = new mysqli($host, $user, $pwd, $sql_db);    // Log in and use database
+    $conn = mysqli_connect($host, $user, $pwd, $sql_db);    // Log in and use database
 
     //connection to database
     require_once "settings.php";
     $conn = mysqli_connect($host, $user, $pwd, $sql_db);
-    
+
     session_start();
-    
     $error = "";
     if (isset($_POST["submit"])) {
         if (empty($_POST["username"]) || empty($_POST["password"])) {
             $error = "Username or Password is Invalid";
-            
         } else {
             //define username and password
             $username = $_POST["username"];
             $password = $_POST["password"];
+            //to prevent MySql injection
+            $username = sanitise_input($username);
+            $password = sanitise_input($password);
 
-            $query = "SELECT * FROM Login_ WHERE password_='$password' AND username ='$username' ";
-            $result = mysqli_query($conn, $query);
-
-            
-
+            $query = "SELECT * FROM userlogin WHERE userpassword='$password' AND username ='$username' ";
             $result = mysqli_query($conn, $query);
 
             if ($result) { //if successful
                 $_SESSION['login_user'] = $username; // Initializing Session
-                header("location: index.php"); //redirecting to addsales page. 
+                header("location: homepage.php"); //redirecting to manager page. 
                 exit();
             } else {
                 $error = "Username or Password is invalid";
@@ -76,18 +80,18 @@
 
     <div class="login-form">
         <h3>Please Login</h3>
-        <form method="post" action="AddSales.html">
+        <form method="post" action="index.php">
             <fieldset>
                 <p>
                     <label for="name">Username :</label>
-                    <input type="text" id="name" name="username" placeholder="username" />
+                    <input type="text" id="username" name="username" placeholder="username" />
                 </p>
                 <p>
                     <label for="password">Password :</label>
                     <input type="password" id="password" name="password" placeholder="**********" />
                 </p>
                 <p>
-                    <input name="submit" type="submit" value=" Login " />
+                    <input name="submit" type="submit" value="Login " />
                 <p>Don't have Account? <a href="signup.php">Sign Up here</a></p>
                 </p>
 
