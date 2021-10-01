ca24c032e83eceb706f88952c442e276256741cf
Pum Suan Khai SEKTAK <josephkhaipi@gmail.com>
2021-10-01 12:58:23 +1000

    Add Admin Page

--- SalesReport.php
+++ SalesReport.php
@@ -24,7 +24,7 @@
 	<div class="stockReport">
 
 		<!--for Stock report-->
-		<h3>Stock Report</h3>
+		<h3>Stock Report will display available stock</h3>
 
 		<?php
 
@@ -35,7 +35,7 @@
 
 			//echo "database connected!<br>";
 
-			$query = "SELECT * FROM PHPWarehouse;";
+			$query = "SELECT * FROM PHPWarehouse WHERE ItemStatus = 'INSTOCK';";
 
 			$result = mysqli_query($conn, $query);
 			if ($result) {	//   query was successfully executed
@@ -52,7 +52,7 @@
 						<th><a class="colum_sort" id="description" data-order="'.$order.'">Ordertime</a></th>
 						<th><a class="colum_sort" id="description" data-order="'.$order.'">ItemStatus</a></th>
 						<th><a class="colum_sort" id="price" data-order="'.$order.'">Price</a></th>
-						<th>Action</th>
+
 					</tr>
 		<?php
 					while ($record) {
@@ -63,7 +63,7 @@
 						echo "<td>{$record['Ordertime']}</td>";
 						echo "<td>{$record['ItemStatus']}</td>";
 						echo "<td>{$record['Price']}</td>";
-						echo "<td><a href='delete.php?id=" . $record['Item_ID'] . "'>Delete</a></td>				
+						echo "				
 					</tr>";
 
 						$record = mysqli_fetch_assoc($result);
@@ -90,13 +90,12 @@
 	<hr />
 	<br />
 	<br />
-	<br />
-	<br />
+
 
 	<div class="saleReport">
 
 		<!--for Stock report-->
-		<h3>Sales Report</h3>
+		<h3>Sales Report will display sold item</h3>
 
 		<?php
 
@@ -107,7 +106,7 @@
 
 			//echo "database connected!<br>";
 
-			$query = "SELECT * FROM PHPWarehouse;";
+			$query = "SELECT * FROM PHPWarehouse WHERE ItemStatus = 'SOLD';";
 
 			$result = mysqli_query($conn, $query);
 			if ($result) {	//   query was successfully executed
@@ -124,7 +123,7 @@
 						<th><a class="colum_sort" id="description" data-order="'.$order.'">Ordertime</a></th>
 						<th><a class="colum_sort" id="description" data-order="'.$order.'">ItemStatus</a></th>
 						<th><a class="colum_sort" id="price" data-order="'.$order.'">Price</a></th>
-						<th>Action</th>
+
 					</tr>
 		<?php
 					while ($record) {
@@ -135,7 +134,79 @@
 						echo "<td>{$record['Ordertime']}</td>";
 						echo "<td>{$record['ItemStatus']}</td>";
 						echo "<td>{$record['Price']}</td>";
-						echo "<td><a href='delete.php?id=" . $record['Item_ID'] . "'>Delete</a></td>				
+						echo "			
+					</tr>";
+
+						$record = mysqli_fetch_assoc($result);
+					}
+					echo "</table>";
+					mysqli_free_result($result);	// Free resources
+				} else {
+					echo "<p>No record retrieved.</p>";
+				}
+			} else {
+				echo "<p>Orders table doesn't exist or select operation unsuccessful.</p>";
+			}
+			mysqli_close($conn);	// Close the database connection
+		} else {
+			echo "<p>Unable to connect to the database.</p>";
+		}
+
+		?>
+
+		<input type="button" name="generateSalesCSV" value="Generate CSV" />
+
+	</div>
+
+
+	<hr />
+	<br />
+	<br />
+
+
+	<div class="outofStockReport">
+
+		<!--for Stock report-->
+		<h3>Sales Report will display out of stock item</h3>
+
+		<?php
+
+		require_once "settings.php";	// Load MySQL log in credentials
+		$conn = mysqli_connect($host, $user, $pwd, $sql_db);	// Log in and use database
+
+		if ($conn) { // connected
+
+			//echo "database connected!<br>";
+
+			$query = "SELECT * FROM PHPWarehouse WHERE ItemStatus = 'OUT_OF_STOCK';";
+
+			$result = mysqli_query($conn, $query);
+			if ($result) {	//   query was successfully executed
+
+				$record = mysqli_fetch_assoc($result);
+				if ($record) {		//   record exist
+					echo "<table class='salesReportTable'>";
+		?>
+					<tr>
+						<th><a class="colum_sort" id="id" data-order="'.$order.'">Item_ID</a></th>
+						<th><a class="colum_sort" id="name" data-order="'.$order.'">Item_Name</a></th>
+						<th><a class="colum_sort" id="quantity" data-order="'.$order.'">Quantity</a></th>
+						<th><a class="colum_sort" id="description" data-order="'.$order.'">Item_Description</a></th>
+						<th><a class="colum_sort" id="description" data-order="'.$order.'">Ordertime</a></th>
+						<th><a class="colum_sort" id="description" data-order="'.$order.'">ItemStatus</a></th>
+						<th><a class="colum_sort" id="price" data-order="'.$order.'">Price</a></th>
+
+					</tr>
+		<?php
+					while ($record) {
+						echo "<tr><td>{$record['Item_ID']}</td>";
+						echo "<td>{$record['Item_Name']}</td>";
+						echo "<td>{$record['Quantity']}</td>";
+						echo "<td>{$record['Item_Description']}</td>";
+						echo "<td>{$record['Ordertime']}</td>";
+						echo "<td>{$record['ItemStatus']}</td>";
+						echo "<td>{$record['Price']}</td>";
+						echo "			
 					</tr>";
 
 						$record = mysqli_fetch_assoc($result);
--- admin.php
+++ admin.php
@@ -19,7 +19,153 @@
 
 
 
+    <fieldset>
+        <!--Search Item-->
+        <form action="admin.php" method="post">
+            <p><strong>Search Item</strong></p>
+            <p><label>Search: <input type="text" name="searchq" id="searchq"></label>
+                <label>Search by: <select name="searchopt" id="searchopt">
+                        <option></option>
+                        <option value="item_name">Item Name</option>
+                        <option value="item_status">Item Status</option>
+                    </select></label>
+                <input type="submit" name="submit" value="Search">
+            </p>
 
+        </form>
+
+        <hr />
+        <br />
+        <br />
+
+
+        <!--Update Item status form-->
+        <form action="admin.php" method="post">
+            <p><strong>Update Item Status</strong> </p>
+            <p>
+                <label>Item ID: <input type="text" name="item_id" id="item_id"></label>
+                <select name="update_Opt">
+                    <option value="IN_STOCK">IN STOCK</option>
+                    <option value="OUT_OF_STOCK">OUT OF STOCK</option>
+                    <option value="SOLD">SOLD</option>
+                </select>
+                <input type="submit" name="update" value="update" />
+            </p>
+
+        </form>
+    </fieldset>
+
+    <?php
+
+
+    $query = "";
+
+    require_once "settings.php";    // Load MySQL log in credentials
+    $conn = mysqli_connect($host, $user, $pwd, $sql_db);    // Log in and use database
+
+    if (isset($_POST["searchq"]) && $_POST["searchq"] != "") {
+        if (isset($_POST["searchopt"]) && $_POST["searchopt"] != "") {
+
+            $escsearch = mysqli_real_escape_string($conn, $_POST["searchq"]);
+            $searchval = preg_replace("#[^a-z 0-9]#i", '', $escsearch);
+            $opt = trim($_POST["searchopt"]);
+
+            switch ($opt) {
+
+                case "item_name":
+                    $query = "SELECT * FROM PHPWarehouse WHERE Item_Name LIKE '%$searchval%' ";
+
+                    break;
+                case "item_status":
+                    $query = "SELECT * FROM PHPWarehouse WHERE ItemStatus LIKE '%$searchval%' ";
+
+                    break;
+            }
+        }
+    } else {
+        $query = "SELECT * FROM PHPWarehouse;";
+    }
+
+
+    //Update Item Status
+    if (isset($_POST["item_id"]) && $_POST["item_id"] != "") {
+        if (isset($_POST["update_Opt"]) && $_POST["update_Opt"] != "") {
+
+            $item_id_val = trim($_POST["item_id"]);
+
+            $opt = trim($_POST["update_Opt"]);
+            switch ($opt) {
+                case "Pending":
+                    $query = "UPDATE PHPWarehouse SET ItemStatus ='INSTOCK' WHERE Item_ID= $item_id_val;";
+                    break;
+                case "Fullfilled":
+                    $query = "UPDATE PHPWarehouse SET ItemStatus ='OUT_OF_STOCK' WHERE Item_ID= $item_id_val;";
+                    break;
+                case "Paid":
+                    $query = "UPDATE PHPWarehouse SET ItemStatus ='SOLD' WHERE Item_ID= $item_id_val;";
+                    break;
+            }
+
+            $result = mysqli_query($conn, $query);
+            if ($result) {
+                echo "<p>Update successful!</p>";
+                $query = "SELECT * FROM PHPWarehouse;";
+            } else {
+                echo "<p>Update not successful!</p>";
+            }
+        }
+    }
+
+
+    if ($conn) { // connected
+
+        $result = mysqli_query($conn, $query);
+        if ($result) {    //   query was successfully executed
+
+            $record = mysqli_fetch_assoc($result);
+            if ($record) {        //   record exist
+                echo "<table class='salesReportTable'>";
+    ?>
+                <tr>
+                    <th><a class="colum_sort" id="id" data-order="'.$order.'">Item_ID</a></th>
+                    <th><a class="colum_sort" id="name" data-order="'.$order.'">Item_Name</a></th>
+                    <th><a class="colum_sort" id="quantity" data-order="'.$order.'">Quantity</a></th>
+                    <th><a class="colum_sort" id="description" data-order="'.$order.'">Item_Description</a></th>
+                    <th><a class="colum_sort" id="description" data-order="'.$order.'">Ordertime</a></th>
+                    <th><a class="colum_sort" id="description" data-order="'.$order.'">ItemStatus</a></th>
+                    <th><a class="colum_sort" id="price" data-order="'.$order.'">Price</a></th>
+                    <th>Action</th>
+
+                </tr>
+    <?php
+                while ($record) {
+                    echo "<tr><td>{$record['Item_ID']}</td>";
+                    echo "<td>{$record['Item_Name']}</td>";
+                    echo "<td>{$record['Quantity']}</td>";
+                    echo "<td>{$record['Item_Description']}</td>";
+                    echo "<td>{$record['Ordertime']}</td>";
+                    echo "<td>{$record['ItemStatus']}</td>";
+                    echo "<td>{$record['Price']}</td>";
+                    echo "<td><a href='delete.php?id=" . $record['Item_ID'] . "'>Delete</a></td>;
+                    		
+					</tr>";
+
+                    $record = mysqli_fetch_assoc($result);
+                }
+                echo "</table>";
+                mysqli_free_result($result);    // Free resources
+            } else {
+                echo "<p>No record retrieved.</p>";
+            }
+        } else {
+            echo "<p>Orders table doesn't exist or select operation unsuccessful.</p>";
+        }
+        mysqli_close($conn);    // Close the database connection
+    } else {
+        echo "<p>Unable to connect to the database.</p>";
+    }
+
+    ?>
 
 
 
