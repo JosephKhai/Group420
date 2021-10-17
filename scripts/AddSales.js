4abbc0d814ebf727bbceec3730d20a1333d1a8b7
DavidNG <brokenarma@gmail.com>
2021-10-18 04:59:40 +1100

    Add sales functionality
    Fully completed add sales functionality
    sales_process.php added

--- scripts/AddSales.js
+++ scripts/AddSales.js
@@ -1,119 +1,148 @@
-function CheckforInt(var s) {
-		return Number.isInteger(s);
-} 
-
-function Add() { 
-	// Find a <table> element with id="OrderTable"
-	var table = document.getElementById("OrderTable");
-	var itemname = document.getElementsByName("ItemName")[0].value; 
-	var quantity = document.getElementsByName("Quantity")[0].value;
-	var RowCount = table.tBodies[0].rows.length; //table.tBodies.length;
-	var itemPrice = "0";
-	//PreparedStatement preparedStmt;
-	//ResultSet rs;
-	
-	
-	var user = "vapenation4lyf"; 
-	var pass = "LLKjMwLSYFW44dbF";
-	var serverDetails = "group420.cguno6verhdn.ap-southeast-2.rds.amazonaws.com";
-	var portNum = "3306";
-	//var connect = getConnection(serverDetails, user, pass, portNum);
-	//String query = " insert into users (first_name, last_name, date_created, is_admin, num_points)" + " values (?, ?, ?, ?, ?)";
-	
-	// Create an empty <tr> element and add it to the 1st position of the table:
-	var row = table.insertRow(RowCount);
-	
-	// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-	var itemCell = row.insertCell(0);
-	var quantCell = row.insertCell(1);
-	var priceCell = row.insertCell(2);
-	var totalCell = row.insertCell(3);
-	
-	//var mysql = require('mysql');
-	
-	//var con = mysql.createConnction({
-	//	host: serverDetails
-	//	user: user
-	//	password: pass
-	//})
-	//
-	//con.connect(function(err) {
-	//	if (err) throw err;
-	//	console.log("Connected!");
-	//});
-	//
-	//con.query("SELECT Price FROM Warehouse WHERE UPPER(Item_Name) = ?", toUpperCase(itemname), function (err, result) {
-	//	if (err) throw err;
-	//	console.log(result);
-	//});
-	
-	// Add some text to the new cells:
-	itemCell.innerHTML = itemname;
-	quantCell.innerHTML = quantity;
-	
-	//TODO check price here
-	//if(connect != null) {
-	//	// the mysql insert statement
-	//	query = "SELECT Price FROM Warehouse WHERE UPPER(Item_Name) = ?";
-	//	
-	//	// create the mysql insert preparedstatement
-	//	preparedStmt = conn.prepareStatement(query);
-	//	preparedStmt.setString (1, itemname);
-	//	rs = preparedStmt.executeQuery();        // Get the result table from the query 
-	//	itemPrice = rs.getString(1);        // Retrieve the first column value
-	//}
-	
-		// check if itemprice is valid
-	if (CheckforInt(itemPrice)) {
-		priceCell.innerHTML = itemPrice;
-		totalCell.innerHTML = itemPrice * quantity;
-	}
-	else { 
-		priceCell.innerHTML = "Error";
-		priceCell.innerHTML = "NaN";
-	}
+function CheckforInt(number) {
+		return Number.isInteger(number);
 }
 
-//function ProcessItems(){
-//	
-//	var table = document.getElementById("OrderTable");
-//	var itemname = document.getElementsByName("ItemName")[0].value; 
-//	var quantity = document.getElementsByName("Quantity")[0].value;
-//	
-//	for (var i = 0, row; row = table.rows[i]; i++) {
-//		//iterate through rows
-//		
-//
-//		//for (var j = 0, col; col = row.cells[j]; j++) {
-//     //iterate through columns
-//		//
-//		//}  
-//	}
-//}
+function parseQuant(number)
+{
+	result = false;
+	if(CheckforInt(parseInt(number)))
+	{
+		if(number > 0 && number < 1000)
+		{
+			return true;
+		}
+	}
+	
+	return result;
+}
 
-//public function Connection getConnection(var serverDeets, var usr, var pwd, var port) throws SQLException {
-//
-//    Connection conn = null;
-//    Properties connectionProps = new Properties();
-//    connectionProps.put("user", usr);
-//    connectionProps.put("password", pwd);
-//
-//    if (this.dbms.equals("mysql")) {
-//        conn = DriverManager.getConnection(
-//                   "jdbc:" + mysql + "://" +
-//                   serverDeets +
-//                   ":" + port + "/",
-//                   connectionProps);
-//    } else {
-//		return null;
-//    }
-//    System.out.println("Connected to database");
-//    return conn;
-//}
+function Add() {
+	// Find a <table> element with id="OrderTable"
+	var table = document.getElementById("OrderTable");
+	var itemname = document.getElementsByName("productOp")[0].value; 
+	var quantity = document.getElementById("Quantity").value;
+	var RowCount = table.tBodies[0].rows.length; //table.tBodies.length;
+	var itemPrice = "0";
+	
+	var result = true;
+	var errMsg = "";
+	
+	
+	if(!parseQuant(parseInt(quantity))){
+		errMsg += "Quantity cannot be zero and must be between 1-999.\n";
+	}
+	else {
+		if(itemname == ""){
+			errMsg += "Please select an item!.\n";
+			alert (errMsg);
+			result = false;
+			return result;
+		}
+
+		// Create an empty <tr> element and add it to the 1st position of the table:
+		var row = table.insertRow(RowCount);
+	
+		// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
+		var itemCell = row.insertCell(0);
+		var quantCell = row.insertCell(1);
+		var priceCell = row.insertCell(2);
+		var totalCell = row.insertCell(3);
+	
+		// Add some text to the new cells:
+		itemCell.innerHTML = itemname;
+		quantCell.innerHTML = quantity;
+	
+		//TODO check price here
+		itemPrice = myPrices.get(itemname);
+	
+			// check if itemprice is valid
+		if (CheckforInt(parseInt(itemPrice))) {
+			priceCell.innerHTML = "$" + itemPrice;
+			totalCell.innerHTML = "$" + (itemPrice * quantity);
+		}
+		else { 
+			priceCell.innerHTML = "Error";
+			priceCell.innerHTML = "NaN";
+		}
+	}
+	
+	if (errMsg != "") {
+		/* Display error message any error(s) is/are detected */
+		alert (errMsg);
+		result = false;
+	}
+	
+	return result;
+}
+
+function ProcessItems(){
+	
+	var result = true;
+	var errMsg = "";
+	var price;
+	var table = document.getElementById("OrderTable");
+	var RowCount = table.tBodies[0].rows.length; //table.tBodies.length;
+	
+	if(RowCount < 2)
+	{
+		errMsg += "There must at least be one sale to add!!";
+	}
+	else
+	{
+		//var theItems = [
+		//	{
+		//		"name": table.rows[1].cells[0].innerHTML,
+		//		"quantity": table.rows[1].cells[1].innerHTML,
+		//		"price": table.rows[1].cells[2].innerHTML
+		//	}
+		//];
+		
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", "sales_process.php");
+		xhr.setRequestHeader("Accept", "application/json");
+		xhr.setRequestHeader("Content-Type", "application/json");
+		
+		xhr.onreadystatechange = function () {
+		if (xhr.readyState === 4) {
+			console.log(xhr.status);
+			console.log(xhr.responseText);
+		}};
+		
+		var theItems = [];
+			
+		for(var i = 1; i < RowCount; i++) // loop through the rows 
+		{
+			price = table.rows[i].cells[2].innerHTML;
+			price = price.replace("$",""); // remove the $
+			
+			theItems.push({
+				"name": table.rows[i].cells[0].innerHTML,
+				"quantity": table.rows[i].cells[1].innerHTML,
+				"price": price});
+		}
+		
+		var data = JSON.stringify(theItems);
+		
+		xhr.send(data);
+		
+		console.log(JSON.stringify(theItems));
+	}
+	
+	if (errMsg != "") {
+		/* Display error message any error(s) is/are detected */
+		alert (errMsg);
+		result = false;
+	}
+	
+	return result;
+
+}
 
 function init() {
 	//Setup();
 	var addbutton = document.getElementById("addBtn");
+	var probutton = document.getElementById("processBtn");
 	addbutton.onclick = Add;
+	probutton.onclick = ProcessItems;
 }
 window.onload = init;
